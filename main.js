881087029e54d1e497fd2371f35da81e083070d2
Edvard A. M. Haaland <fishurban@gmail.com>
2017-02-28 22:09:33 +0100

    array converter
    array converter

---
+++ main.js
@@ -0,0 +1,16 @@
+// Use to make a textbox of words return as array-compatible text
+// because I'm too lazy to put in all the names into my dnd randomizer.
+function NamesToArray() {
+	var inputText = document.getElementById('arrayInput').value;
+	document.getElementById('arrayOutput').innerHTML = '[ "';
+	for (var i = 0;i<inputText.length;i++) {
+		var letter = inputText.charAt(i);
+		if (letter == " ") {
+			document.getElementById('arrayOutput').innerHTML += '", "';
+		}
+		else {
+			document.getElementById('arrayOutput').innerHTML += letter;
+		}
+	}
+	document.getElementById('arrayOutput').innerHTML += '" ];';
+}
